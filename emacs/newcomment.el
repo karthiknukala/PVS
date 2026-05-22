c777cf3ee02677aa552f33e2bd1cb5bd0d2f19e9
Karthik Nukala <karthik.nukala@sri.com>
2026-05-21 19:17:48 -0700

    updating BSD 3-Clause license in all lisp file headers

--- emacs/newcomment.el
+++ emacs/newcomment.el
@@ -8,22 +8,16 @@
 ;; Keywords: comment uncomment
 ;; Revision: $Id: newcomment.el,v 1.35.4.5 2002/05/16 16:01:24 eliz Exp $
 
-;; This file is part of GNU Emacs.
-
-;; GNU Emacs is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 2, or (at your option)
-;; any later version.
-
-;; GNU Emacs is distributed in the hope that it will be useful,
+;; --------------------------------------------------------------------
+;; PVS
+;; Copyright (C) 2026, SRI International. All Rights Reserved.
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the 3-Clause BSD License.
+;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs; see the file COPYING.  If not, write to the
-;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-;; Boston, MA 02111-1307, USA.
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; 3-Clause BSD License for more details.
+;; --------------------------------------------------------------------
 
 ;;; Commentary:
 
